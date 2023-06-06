315f8f8291ce72ce9b6ddd68e2938ba00a08e1e4
tsukki9696 <viniciusdesouzagvitorino@gmail.com>
2023-06-06 20:05:56 -0300

    removed html folder, gonna be sent to a separate repo

--- html/landing-page/script.js
+++
@@ -1,6 +0,0 @@
-const header = document.querySelector('h3'); // selects the h3
-const list = document.querySelector('ul'); // selects the ul
-
-if (element.matches(':hover')){
-  header.classList.toggle('toggled') // changes color of the h3's background.
-}
