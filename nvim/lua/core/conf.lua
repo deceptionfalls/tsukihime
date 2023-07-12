e9a576dd1f87b8b66e319892d377cb9c0ee4b08d
tsukki9696 <viniciusdesouzagvitorino@gmail.com>
2023-07-11 22:32:29 -0300

    merged base nvim config into conf.lua

--- nvim/lua/core/conf.lua
+++ nvim/lua/core/conf.lua
@@ -1,3 +1,14 @@
+-- Legacy Vim settings, includes vimscript stuff
+
+vim.cmd [[
+  " you would normally use `set termguicolors` here but it breaks my pywal theme
+  map <leader>g :nohlsearch<CR>
+  map <leader>w :w<CR>
+  map <leader>c :q<CR>
+  nnoremap <SPACE> <Nop>
+  map <leader>s :setlocal spell! spelllang=en_au<CR>
+]]
+
 -- Lua options
 
 vim.g.mapleader = " "                   -- leader key
